ef959271ce361769a7f2d060b1601de4f85b6d06
subika-2005 <vvsubika@gmail.com>
2025-05-07 10:12:05 +0530

    Update oose.py

--- oose.py
+++ oose.py
@@ -1,6 +1,5 @@
-def factorial_iterative(n):
-    result = 1
-    for i in range(2, n + 1):
-        result *= i
-    return result
-print("Factorial (iterative) of 5:", factorial_iterative(5))
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial_recursive(n - 1)
+print("Factorial (recursive) of 5:", factorial_recursive(5))
